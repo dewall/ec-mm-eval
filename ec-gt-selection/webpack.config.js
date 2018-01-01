11204f634a856a4daefd052df6877cc40565fcc6
dewall <a.dewall@52north.org>
2018-01-01 16:27:25 +0100

    updated webpack config

--- ec-gt-selection/webpack.config.js
+++ ec-gt-selection/webpack.config.js
@@ -1,34 +1,94 @@
 var webpack = require("webpack"),
     path = require("path");
 
+// webpack plugins
+var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin,
+    ExtractTextPlugin = require("extract-text-webpack-plugin"),
+    HtmlWebpackPlugin = require("html-webpack-plugin"),
+    CopyWebpackPlugin = require("copy-webpack-plugin");
+
 var srcPath = path.join(__dirname, "/src/"),
     distPath = path.join(__dirname, "/dist/");
 
 module.exports = {
-    watch: true,
-    cache: true,
-    devtool: '#cheap-module-eval-source-map',
-    context: srcPath,
+    devtool: 'cheap-module-source-map',
+
     entry: {
-        app: './app.ts',
+        polyfills: path.resolve(srcPath, "polyfills.ts"),
+        app: [
+            path.resolve(srcPath, "main.ts"),
+            path.resolve(srcPath, "main.scss")
+        ]
     },
+
     output: {
-        path: distPath,
         filename: '[name].bundle.js',
-        publicPath: "/assets"
+        sourceMapFilename: '[name].map',
+        chunkFilename: '[id].chunk.js',
     },
+
+    devServer: {
+        historyApiFallback: true,
+        stats: "minimal"
+    },
+
     resolve: {
-        modules: ["node_modules"],
-        extensions: [".ts", ".js"]
+        // modules: ["node_modules", "src"],
+        extensions: ['.ts', '.js', '.json', '.css', '.scss', '.html']
     },
+
     module: {
         rules: [
-            { test: /\.ts?$/, loader: "ts-loader" },
-            { test: /\.scss$/, loaders: ["style-loader", "css-loader", "sass-loader"] },
-            { test: /\.(png|jpe?g|gif|svg)(\?.*)?$/, loader: 'url-loader' }
+            {
+                test: /\.ts$/,
+                use: ['awesome-typescript-loader?silent=true', 'angular2-template-loader']
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader',
+                exclude: path.resolve(srcPath, 'index.html')
+            },
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+                exclude: path.resolve(srcPath, 'components')
+            },
+            {
+                test: /\.scss$/,
+                use: ['raw-loader', 'sass-loader'],
+                include: path.resolve(srcPath, 'components')
+            },
+            {
+                test: /\.css$/,
+                use: ['to-string-loader', 'css-loader'],
+                include: path.resolve(__dirname, 'node_modules')
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot)$/,
+                use: 'file-loader?name=assets/[name].[ext]'
+            },
         ]
     },
     plugins: [
-        new webpack.NoEmitOnErrorsPlugin()
+        new ExtractTextPlugin(
+            "[name].css"
+        ),
+        new HtmlWebpackPlugin({
+            template: path.resolve(srcPath, 'index.html'),
+            inject: true
+        }),
+        new webpack.ContextReplacementPlugin(
+            /angular(\\|\/)core(\\|\/)@angular/,
+            srcPath
+        ),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app']
+        }),
+        new webpack.NoEmitOnErrorsPlugin(
+
+        ),
+        new CopyWebpackPlugin([{
+            from: srcPath
+        }])
     ]
 };
