f9ca0ec090d178411536aaf20c0e924a60345b9c
dewall <a.dewall@52north.org>
2018-01-01 18:03:39 +0100

    leaflet basemap added

--- ec-gt-selection/src/app.module.ts
+++ ec-gt-selection/src/app.module.ts
@@ -1,3 +1,9 @@
+import "leaflet";
+import "leaflet/dist/leaflet.css";
+import "zone.js/dist/zone";
+import "zone.js/dist/long-stack-trace-zone";
+import "reflect-metadata";
+
 import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
--- ec-gt-selection/src/components/app.component.ts
+++ ec-gt-selection/src/components/app.component.ts
@@ -1,5 +1,6 @@
-
 import { Component, OnInit } from '@angular/core';
+
+import * as L from "leaflet";
 
 @Component({
     selector: 'app',
@@ -7,11 +8,21 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-    constructor() { 
 
+    private baseMap: any;
+
+    constructor() {
+        this.baseMap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        });
     }
 
-    ngOnInit() { 
+    ngOnInit() {
+        let map = L.map("map", {
+            layers: [this.baseMap],
+        }).setView([51.505, -0.09], 12)
 
+        L.control.scale().addTo(map);
+        L.control.zoom({ position: "topright" }).addTo(map);
     }
 }
